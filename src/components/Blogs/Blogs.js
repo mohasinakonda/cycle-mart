5f030e0350cf1b9661ef6c140a793d1b19048685
mohasin akonda <mohasinakonda39@gmail.com>
2022-04-05 07:53:49 +0600

    second blogs updated

--- src/components/Blogs/Blogs.js
+++ src/components/Blogs/Blogs.js
@@ -6,6 +6,26 @@ const Blogs = () => {
 			<article>
 				<div className="d-flex justify-content-center">
 					<img
+						className="w-75 img-fluid"
+						src="https://imgs.developpaper.com/imgs/2271126660-5d35c5416b4ce_articlex.png"
+						alt=""
+					/>
+				</div>
+				<h2 className="pt-3">what is context API ?</h2>
+				<p>
+					The React Context API is a way for a React app to effectively produce
+					global variables that can be passed around. This is the alternative to
+					"prop drilling" or moving props from grandparent to child to parent,
+					and so on.This feature solve the major problem props drilling. Context
+					is also touted as an easier, lighter approach to state management
+					using Redux. Context API is a new feature added in version 16.3 of
+					React that allows one to share state across the entire app (or part of
+					it) lightly and with ease.
+				</p>
+			</article>
+			<article>
+				<div className="d-flex justify-content-center">
+					<img
 						className="w-75 img-fluid p-2 "
 						src="https://www.thoughtco.com/thmb/lzj0HsBfSYcJkhIma-QaG7NH-v0=/1732x1231/filters:fill(auto,1)/GettyImages-663932038-6a057cdc923147bf99f1cee4654a1032.jpg"
 						alt=""
